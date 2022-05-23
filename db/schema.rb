839dbbd1f356bc7f55837ffab7e986513e1e311f
DnameZ <markomrkonjic479@gmail.com>
2022-05-23 09:47:55 +0000

    updated navbar and user registration and edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_20_222013) do
+ActiveRecord::Schema.define(version: 2022_05_23_001414) do
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 2022_05_20_222013) do
     t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
+    t.string "firstname"
+    t.string "lastname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
